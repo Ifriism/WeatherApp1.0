d5b3fa8ba986147d7bd83b3bf1799ec54a9ba3a3
Ifriism <idafm2@hotmail.com>
2024-11-03 12:18:40 +0100

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+   
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "fea6579f5ctf53bb7491ae80ac32o60f"
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Madrid"); // replaces the HTML city and temperature and defaults the city to Madrid once the page loads
