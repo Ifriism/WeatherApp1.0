2668799ca18381eeddcdcd29eed92f3ee561468b
Ifriism <idafm2@hotmail.com>
2024-11-04 22:52:45 +0100

    injected the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ function refreshWeather(response) {
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`
 }
 
+
 function formatDate(date) {
         let minutes = date.getMinutes();
     let hours = date.getHours();
@@ -55,7 +56,32 @@ let searchInput = document.querySelector("#search-form-input");
 searchCity(searchInput.value);
 }
 
+function displayForecast() {
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+
+    days.forEach(function (day) {
+    forecastHtml = 
+    forecastHtml + 
+        `
+        <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature"><strong>15° </strong></div>
+        <div class="weather-forecast-temperature">9°</div>
+        </div>
+         </div>
+        `;
+});
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Madrid"); // replaces the HTML city and temperature and defaults the city to Madrid once the page loads
+displayForecast();
+
